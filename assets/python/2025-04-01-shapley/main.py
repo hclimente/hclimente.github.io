04a29c27fdf93ef3c98771c50925a0320b46ca5a
Héctor Climente-González <hclimente@gmail.com>
2025-04-14 00:40:35 +0100

    feat: improved main.py

--- assets/python/2025-04-01-shapley/main.py
+++ assets/python/2025-04-01-shapley/main.py
@@ -1,6 +1,52 @@
-def main():
-    print("Hello from 2025-04-01-shapley!")
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: x-VW6uKvAAFJ6FbaR1Atg
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import shap
+import sklearn
+
+X, y = shap.datasets.diabetes()
+X = X.rename(columns={
+    "age": "Age",
+    "sex": "Sex",
+    "bmi": "BMI",
+    "bp": "Blood pressure",
+    "s1": "Total cholesterol",
+    "s2": "LDL cholesterol",
+    "s3": "HDL cholesterol",
+    "s4": "Total cholesterol/HDL ratio",
+    "s5": "Triglycerides",
+    "s6": "Blood sugar",
+})
 
 
-if __name__ == "__main__":
-    main()
+# %%
+# Train a model
+model = sklearn.ensemble.RandomForestRegressor()
+model.fit(X, y)
+
+# %%
+# Explain the model's predictions using SHAP
+explainer = shap.Explainer(model)
+explanation = explainer(X, check_additivity=False)
+
+assert explanation.values.shape == X.shape
+
+# %%
+shap.plots.beeswarm(explanation)
+
+# %%
+shap.plots.waterfall(explanation[0, ...])
