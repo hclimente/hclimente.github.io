b7d382818a0a23c96222212c8a4c2789762859d8
Héctor Climente-González <hclimente@gmail.com>
2025-04-14 01:02:39 +0100

    feat: added code example

---
+++ assets/python/2025-03-15-python-uv/hello-world/main.py
@@ -0,0 +1,24 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: x-VW6uKvAAFJ6FbaR1Atg
+#     language: python
+#     name: python3
+# ---
+
+# %%
+def main():
+    print("Hello from hello-world!")
+
+
+# %%
+if __name__ == "__main__":
+    main()
