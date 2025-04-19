f8d206e37194ee654821b8a23ed003efad1d2363
Héctor Climente-González <hclimente@gmail.com>
2025-04-19 23:54:53 +0100

    feat: last edits

--- assets/python/2025-04-01-shapley/main.py
+++ assets/python/2025-04-01-shapley/main.py
@@ -16,6 +16,7 @@
 
 # %%
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import shap
 import sklearn
@@ -37,6 +38,17 @@ X = X.rename(
     }
 )
 
+
+# %%
+df = X.copy()
+df["Target"] = y
+
+# print df in markdown table format
+print(
+    df[["Age", "Sex", "BMI", "Blood pressure", "Target"]]
+    .head(4)
+    .to_markdown(index=False)
+)
 
 # %%
 # Train a model
@@ -86,3 +98,65 @@ for clust_type, (data, features) in experiment.items():
     plt.ylabel("PC2")
     plt.savefig(f"img/{clust_type.lower()}_pca.jpg", bbox_inches="tight", dpi=300)
     plt.show()
+
+# %% [markdown]
+# # SHAP limitations
+#
+# ## No association
+
+# %%
+np.random.seed(0)
+
+X_sim = np.random.rand(1000, 20)
+y_rand = np.random.rand(1000)
+
+X_sim_tr, X_sim_te, y_rand_tr, y_rand_te = sklearn.model_selection.train_test_split(
+    X_sim, y_rand, train_size=0.9, random_state=0
+)
+
+model = sklearn.ensemble.RandomForestRegressor()
+model.fit(X_sim_tr, y_rand_tr)
+
+explainer = shap.Explainer(model)
+explanation = explainer(X_sim_tr, check_additivity=False)
+
+shap.plots.beeswarm(explanation, show=False)
+plt.savefig("img/beeswarm_random.jpg", bbox_inches="tight", dpi=300)
+plt.show()
+
+# plot prediction on test set
+y_pred = model.predict(X_sim_te)
+plt.figure(figsize=(6, 6))
+# plt.gca().set_axis_off()
+plt.scatter(y_rand_te, y_pred, s=50, alpha=0.5)
+plt.xlabel("True values")
+plt.ylabel("Predicted values")
+plt.title("Random Forest predictions on test set", fontsize=16)
+plt.plot([0, 1], [0, 1], color="red", linestyle="--")
+plt.xlim(0, 1)
+plt.ylim(0, 1)
+plt.savefig("img/random_forest_test.jpg", bbox_inches="tight", dpi=300)
+plt.show()
+
+# %%
+y_intx = np.logical_xor(X_sim[:, 0] > 0.5, X_sim[:, 1] > 0.5)
+
+
+model = sklearn.ensemble.RandomForestClassifier()
+
+model.fit(X_sim, y_intx)
+
+explainer = shap.Explainer(model)
+explanation = explainer(X_sim, check_additivity=False)
+
+shap.plots.beeswarm(explanation[1, :], show=False)
+plt.savefig("img/beeswarm_interaction.jpg", bbox_inches="tight", dpi=300)
+plt.show()
+
+# %%
+explanation
+
+# %%
+explanation
+
+# %%
