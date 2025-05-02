b7867fd338dfd39fd85e464febb4049e3d4dd6ae
Héctor Climente-González <hclimente@gmail.com>
2025-05-03 00:03:54 +0100

    chore: initialize uv project

---
+++ _posts/2025-05-02-hf-transformers.md
@@ -0,0 +1,20 @@
+---
+layout: post
+title: 🤗 transformers
+date: 2025-05-02 11:59:00-0000
+description: XXX
+tags: python machine_learning huggingface
+giscus_comments: true
+related_posts: false
+toc:
+  sidebar: left
+images:
+  compare: true
+  slider: true
+---
+
+Hugging Face 🤗 has become a staple of LLM work.
+
+The `transformers` library is a powerful tool for working with transformer models, providing a wide range of pre-trained models and utilities for fine-tuning and deploying them.
+
+Problem that is solved: different models have different APIs. The `transformers` library provides a unified API for all transformer models, making it easy to switch between them without having to learn a new API each time.
---
+++ assets/python/2025-05-02-hf-transformers/main.py
@@ -0,0 +1,57 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+# ---
+
+# %%
+from transformers import AutoTokenizer, AutoModelForMaskedLM
+import torch
+
+# Import the tokenizer and the model
+tokenizer = AutoTokenizer.from_pretrained(
+    "InstaDeepAI/nucleotide-transformer-v2-500m-multi-species", trust_remote_code=True
+)
+model = AutoModelForMaskedLM.from_pretrained(
+    "InstaDeepAI/nucleotide-transformer-v2-500m-multi-species", trust_remote_code=True
+)
+
+# Choose the length to which the input sequences are padded. By default, the
+# model max length is chosen, but feel free to decrease it as the time taken to
+# obtain the embeddings increases significantly with it.
+max_length = tokenizer.model_max_length
+
+# Create a dummy dna sequence and tokenize it
+sequences = ["ATTCCGATTCCGATTCCG", "ATTTCTCTCTCTCTCTGAGATCGATCGATCGAT"]
+tokens_ids = tokenizer.batch_encode_plus(
+    sequences, return_tensors="pt", padding="max_length", max_length=max_length
+)["input_ids"]
+
+# Compute the embeddings
+attention_mask = tokens_ids != tokenizer.pad_token_id
+torch_outs = model(
+    tokens_ids,
+    attention_mask=attention_mask,
+    encoder_attention_mask=attention_mask,
+    output_hidden_states=True,
+)
+
+# Compute sequences embeddings
+embeddings = torch_outs["hidden_states"][-1].detach().numpy()
+print(f"Embeddings shape: {embeddings.shape}")
+print(f"Embeddings per token: {embeddings}")
+
+# Add embed dimension axis
+attention_mask = torch.unsqueeze(attention_mask, dim=-1)
+
+# Compute mean embeddings per sequence
+mean_sequence_embeddings = torch.sum(attention_mask * embeddings, axis=-2) / torch.sum(
+    attention_mask, axis=1
+)
+print(f"Mean sequence embeddings: {mean_sequence_embeddings}")
