77c687d4df9f3554120706978dcc999748844046
hclimente <hclimente@gmail.com>
2025-05-29 20:26:39 +0000

    Deploying to gh-pages from @ hclimente/hclimente.github.io@bc2bbb8ed0930dc11fdc7fc5814fa6495cf260f4 🚀

--- assets/python/utils.py
+++ assets/python/utils.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import plotly
 
 
 def save_fig(fig, basename):
@@ -9,3 +10,47 @@ def save_fig(fig, basename):
     fig.savefig(f"img/{basename}-1600.webp", bbox_inches="tight", dpi=dpi_1600)
     plt.show()
     plt.close(fig)
+
+
+PLOTLY_AXIS_ATTR_DICT = {
+    "showgrid": False,  # Hide grid lines
+    "zeroline": False,  # Hide zero line
+    "showline": True,  # Hide axis line
+    "ticks": "",  # Hide ticks
+    "showticklabels": False,  # Hide tick labels if you want extreme simplicity
+    "linecolor": "black",  # Line color
+    "linewidth": 1,  # Line width
+}
+
+# Position the legend outside the plot area (right and bottom)
+PLOTLY_LEGEND_ATTR_DICT = {
+    "orientation": "v",  # Vertical legend
+    "yanchor": "middle",  # Anchor point is the bottom of the legend box
+    "y": 0.5,  # Position the bottom of the legend box at "y": 0 (bottom of plot area)
+    "xanchor": "left",  # Anchor point is the left of the legend box
+    "x": 1.02,  # Position the left of the legend box just outside the plot area on the right
+}
+
+
+def save_plotly(
+    fig,
+    basename: str,
+    xaxis_attr_dict: dict,
+    yaxis_attr_dict: dict,
+    legend_attr_dict: dict,
+    colorway=plotly.colors.qualitative.T10,
+):
+
+    fig.update_layout(
+        xaxis=xaxis_attr_dict,
+        yaxis=yaxis_attr_dict,
+        template="plotly_white",  # Clean base
+        margin=dict(
+            l=50, r=180, t=50, b=50
+        ),  # Increase right margin to make space for legend
+        legend=legend_attr_dict,
+        colorway=colorway,
+    )
+
+    fig.show()
+    fig.write_html(f"plotly/{basename}.html", include_plotlyjs="cdn")
